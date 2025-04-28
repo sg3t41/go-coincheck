f43afad801b77aa7761729cbb7555f063ed24bd8
sg3t41 <sg3t41@gmail.com>
2025-04-28 09:36:31 +0900

    dev

---
+++ example/example.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/sg3t41/go-coincheck/external/coincheck"
+)
+
+const (
+	key    = ""
+	secret = ""
+)
+
+func main() {
+	coincheck, err := coincheck.New(key, secret)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*1)
+	defer cancel()
+
+	ac, _ := coincheck.Balance(ctx)
+	ac.Print()
+}
