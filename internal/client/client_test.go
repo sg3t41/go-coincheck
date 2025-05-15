cd82d1cbdf74a9a65822e97d47eade9b878522de
sg3t41 <sg3t41@gmail.com>
2025-05-15 16:01:03 +0900

    dev

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -1,1 +1,15 @@
-package client_test
+package client
+
+import (
+	"testing"
+)
+
+func TestNewClient(t *testing.T) {
+	client, err := New("", "")
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if client == nil {
+		t.Errorf("expected client, got nil")
+	}
+}
