7f7a10522318014010a5e2339f0c0f8d9dfb17ef
sg3t41 <sg3t41@gmail.com>
2025-05-25 00:03:18 +0900

    dev

--- external/coincheck/ws/orderbook.go
+++
@@ -1,9 +0,0 @@
-package ws
-
-import (
-	"context"
-)
-
-func (ws *ws) Orderbook(ctx context.Context, pair string) (<-chan string, error) {
-	return ws.orderbook.Subscribe(ctx, pair)
-}
--- external/coincheck/ws/trades.go
+++
@@ -1,9 +0,0 @@
-package ws
-
-import (
-	"context"
-)
-
-func (ws *ws) Trades(ctx context.Context, pair string) (<-chan string, error) {
-	return ws.trades.Subscribe(ctx, pair)
-}
